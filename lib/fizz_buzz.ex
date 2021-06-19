128c8f6bf357c972c134a0584595a12e8eb9255a
Camille <camille_bettini@icloud.com>
2021-06-19 19:46:36 -0300

    add fizzbuzz logic

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,33 @@
 defmodule FizzBuzz do
   @moduledoc """
-  Documentation for `FizzBuzz`.
+  `FizzBuzz` module.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build_list(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_result()
   end
+
+  defp handle_file_result({:ok, file}) do
+    result =
+      file
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, result}
+  end
+
+  defp handle_file_result({:error, reason}), do: {:error, "Error reading file: #{reason}"}
+
+  defp convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_number()
+  end
+
+  defp evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_number(number), do: number
 end
